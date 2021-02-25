797623cc307190eee4601cf6bb073e770b40a095
Saulios <39069766+Saulios@users.noreply.github.com>
2021-02-25 17:48:54 +0100

    initial

---
+++ awa_key_checker.js
@@ -0,0 +1,15 @@
+var country_without_keys = []
+
+for (var country in countryKeys) {
+    if (countryKeys[country]["normal"].length === 0) {
+        country_without_keys.push(country);
+    }
+    for (var level in countryKeys[country]["normal"]) {
+        console.log("Country code: " + country);
+        console.log("Level: " + level + " - Keys: " + countryKeys[country]["normal"][level]);
+    for (var level in countryKeys[country]["prestige"]) {
+        console.log("Level: " + level + " - Keys: " + countryKeys[country]["prestige"][level]);
+    }
+    }
+}
+console.log("Countries without keys: " + country_without_keys);
